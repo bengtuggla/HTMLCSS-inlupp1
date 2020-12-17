8728829dc9d5e7a61508a6b5a15dc5cadd1c0a66
Bengt Uggla <bengt.uggla@gmail.com>
2020-12-17 17:25:58 +0100

    Logo size smaller at MQ 992. Trying to get the shadow mans elbow outside box

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@
 const yearNow = new Date().getFullYear();
 // Hittade non breaking space (\u00A0 ) efter mycket letande
 document.getElementById("footer").innerHTML = ` \u00A0 ${yearNow}\u00A0 ` ;
+
